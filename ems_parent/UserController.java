4475bfbb49422738675b0bb1ebf9dae05b9599df
wyz <781593022@qq.com>
2021-03-07 18:30:10 +0800

    java

---
+++ ems_parent/UserController.java
@@ -0,0 +1,25 @@
+package cn.edu.zstu.controller;
+
+import cn.edu.zstu.util.VerifyCodeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @Author wyz
+ * @Date 2021/3/6 10:49
+ * @Version 1.0
+ * @Description
+ */
+@RestController
+@Slf4j
+public class UserController {
+    // public Map<String, Object> getImage(){
+    //     //1.生成验证码
+    //     String code = VerifyCodeUtil.generateVerifyCode(4);
+    //     //2.存储验证码 redis
+    //
+    //     //3.base64转换
+    // }
+}
